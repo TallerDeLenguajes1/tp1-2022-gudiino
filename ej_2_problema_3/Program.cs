1101245861cde29ace11d1ea5814fc8a254be9c5
gudiino <gudino1387@gmail.com>
2022-08-28 11:07:35 -0300

    cambios en el problema 3

--- ej_2_problema_3/Program.cs
+++ ej_2_problema_3/Program.cs
@@ -9,20 +9,24 @@ namespace Problema03
             try
             {
                 int kilometros, litros_combustible;//para generar excepacion
+                Console.WriteLine("CALCULO DEL RENDIMIENTO DEL COMBUSTIBLE DEL AUTO POR KILOMETRO RECORRIDO");
                 Console.WriteLine("Ingrese los kilometros recorridos");
+                Console.Write("KM = ");
                 kilometros=Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Ingrese la cantidad de litros consumidos");
+                Console.Write("LTS = ");
                 litros_combustible=Convert.ToInt32(Console.ReadLine());
                 double consumoXkilometro;
                 consumoXkilometro=rendimiento(kilometros,litros_combustible);
-                Console.WriteLine("Rendimiento [km/litro] = {0}", consumoXkilometro);
+                Console.WriteLine("Rendimiento [km/lts] = {0}", consumoXkilometro);
                 Console.WriteLine("FIN CODIGO.");
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Datos igresado no valido o falla en el sistema");
+                Console.WriteLine("MENSAJE EXCEPCION:");
                 Console.WriteLine(ex.Message);
-                Console.WriteLine("FIN FALLA.");
+                Console.WriteLine("FIN EXCEPCION.");
             }
             finally
             {
