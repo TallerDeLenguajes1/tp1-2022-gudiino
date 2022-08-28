a9ad52fa93f573cbefd3e8e2e75d803d8da6d5d5
gudiino <gudino1387@gmail.com>
2022-08-28 11:00:41 -0300

    actualizacion problema 2

--- ej_2_problema_2/Program.cs
+++ ej_2_problema_2/Program.cs
@@ -8,18 +8,23 @@ namespace Problema02
         {
             try
             {
-                double num1, num2;
-                Console.WriteLine("Ingrese dos números enteros");
+                int num1, num2;//solicitud de carga de enteros para generar excepcion en division
+                Console.WriteLine("Ingrese dos números");
+                //se ingresa como prueba un numero grande, letras, numero 0 como divisor, numero decimal
+                Console.Write("Número 1 = ");
                 num1=Convert.ToInt32(Console.ReadLine());
-                num2=Convert.ToInt32(Console.ReadLine());//se ingresa como prueba un numero grande
+                Console.Write("Número 2 = ");
+                num2=Convert.ToInt32(Console.ReadLine());
                 double division=num1/num2;
                 Console.WriteLine("Division de los números = {0}", division);
-                Console.WriteLine("FIN.");
+                Console.WriteLine("FIN PROGRAMA.");
             }
-            catch (System.Exception)
+            catch (Exception ex)
             {
                 Console.WriteLine("Datos igresado no valido o falla en el sistema");
-                Console.WriteLine("FIN.");
+                Console.WriteLine("MENSAJE EXCEPCION:");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("FIN EXCEPCION.");
             }   
         }
     }
